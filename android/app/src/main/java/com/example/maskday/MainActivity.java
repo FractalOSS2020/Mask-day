e48fa2151768dde28b708677fb12d312c103a4c4
Dohyun Lee <xxrcy32@gmail.com>
2020-05-22 15:15:26 +0900

    정리

--- android/app/src/main/java/com/example/maskday/MainActivity.java
+++ android/app/src/main/java/com/example/maskday/MainActivity.java
@@ -1,9 +1,5 @@
 package com.example.maskday;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
-
-import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -11,10 +7,20 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import java.util.Calendar;
 
@@ -23,27 +29,15 @@ public class MainActivity extends AppCompatActivity {
     public static final String CHANNEL_ID = "10001";
 
 
-import android.view.KeyEvent;
-import android.view.MotionEvent;
-import android.webkit.WebChromeClient;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.ImageView;
-
-import androidx.annotation.NonNull;
-import androidx.drawerlayout.widget.DrawerLayout;
-
-public class MainActivity extends AppCompatActivity {
-
     private DrawerLayout drawerLayout;
     private View drawerView;
     private WebView webView;
-    private String url="https://www.naver.com/";
+    private String url = "https://www.naver.com/";
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
 
-        if ((keyCode == KeyEvent.KEYCODE_BACK)&&webView.canGoBack()){
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
             webView.goBack();
             return true;
         }
@@ -58,28 +52,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        TextView textView = findViewById(R.id.hello);
-        textView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, SettingActivity.class);
-                startActivity(intent);
-            }
-        });
+//        TextView textView = findViewById(R.id.hello);
+//        textView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this, SettingActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        Button button = (Button) findViewById(R.id.button);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                notification();
+//
+//            }
+//        });
 
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                notification();
-
-        webView = (WebView)findViewById(R.id.webView);
+        webView = (WebView) findViewById(R.id.webView);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl(url);
         webView.setWebChromeClient(new WebChromeClient());
         webView.setWebViewClient(new WebViewClientClass());
-
-
 
 
         ImageView menu = (ImageView) findViewById(R.id.menu);
@@ -92,10 +87,8 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-
-        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
-        drawerView = (View)findViewById(R.id.drawer);
-
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerView = (View) findViewById(R.id.drawer);
 
 
         drawerLayout.setDrawerListener(listener);
@@ -106,44 +99,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-          
-    public void notification(){
-
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.HOUR_OF_DAY, 16);
-        calendar.set(Calendar.MINUTE, 5);
-        calendar.set(Calendar.SECOND, 0);
-
-        NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
-
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
-                .setSmallIcon(R.drawable.applogo)
-                .setContentTitle("올 때 마스크")
-                .setContentText("마스크 구매일입니다! 잊지말고 마스크를 구매해주세요 :)")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setContentIntent(pendingIntent)
-                .setWhen(calendar.getTimeInMillis())
-                .setAutoCancel(true);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-            builder.setSmallIcon(R.drawable.applogo);
-            CharSequence charSequence = "Notification Channel";
-            String description = "It's for over Oreo";
-            int importance = NotificationManager.IMPORTANCE_HIGH;
-
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, charSequence, importance);
-            channel.setDescription(description);
-
-            assert notificationManager != null;
-            notificationManager.createNotificationChannel(channel);
-        } else builder.setSmallIcon(R.drawable.applogo);
-
-        assert notificationManager != null;
-        notificationManager.notify(1234, builder.build());
 
     DrawerLayout.DrawerListener listener = new DrawerLayout.DrawerListener() {
         @Override
@@ -167,6 +122,46 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+
+    public void notification() {
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, 16);
+        calendar.set(Calendar.MINUTE, 5);
+        calendar.set(Calendar.SECOND, 0);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.applogo)
+                .setContentTitle("올 때 마스크")
+                .setContentText("마스크 구매일입니다! 잊지말고 마스크를 구매해주세요 :)")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setWhen(calendar.getTimeInMillis())
+                .setAutoCancel(true);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            builder.setSmallIcon(R.drawable.applogo);
+            CharSequence charSequence = "Notification Channel";
+            String description = "It's for over Oreo";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, charSequence, importance);
+            channel.setDescription(description);
+
+            assert notificationManager != null;
+            notificationManager.createNotificationChannel(channel);
+        } else builder.setSmallIcon(R.drawable.applogo);
+
+        assert notificationManager != null;
+        notificationManager.notify(1234, builder.build());
+
+    }
 
     private class WebViewClientClass extends WebViewClient {
         @Override
