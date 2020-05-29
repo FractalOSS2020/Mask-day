0432ff0f9f9e79e842385aae44cdafd7705cc036
Dohyun Lee <xxrcy32@gmail.com>
2020-05-29 20:56:45 +0900

    출생연도 제거 / 커뮤니티 메뉴 추가

--- android/app/src/main/java/com/example/maskday/Activity/MainActivity.java
+++ android/app/src/main/java/com/example/maskday/Activity/MainActivity.java
@@ -1,31 +1,23 @@
-package com.example.maskday;
+package com.example.maskday.Activity;
 
 import android.Manifest;
 import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.ContentResolver;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.graphics.drawable.ColorDrawable;
 import android.location.LocationManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.Window;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.NumberPicker;
@@ -35,16 +27,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.core.app.NotificationCompat;
 import androidx.core.content.ContextCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.example.maskday.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-import java.util.Calendar;
-
-import io.realm.Realm;
-import io.realm.RealmResults;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -58,11 +45,9 @@ public class MainActivity extends AppCompatActivity {
     private View drawerView;
     private WebView webView;
     private String url = "https://fractaloss2020.github.io/";
-    private LinearLayout pushAlarmLayout, inputLayout, appInfoLayout, maskCheckLayout;
+    private LinearLayout pushAlarmLayout, communityLayout, appInfoLayout, maskCheckLayout;
     private ImageView menu;
     private FloatingActionButton floatingActionButton;
-    private Realm realm;
-    private RealmResults<UserModel> userData;
 
 
     @Override
@@ -75,7 +60,6 @@ public class MainActivity extends AppCompatActivity {
         } else {
             checkRunTimePermission();
         }
-        Realm.init(this);
         init();
 
         webView.getSettings().setJavaScriptEnabled(true);
@@ -107,11 +91,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        /* 출생년도 저장장 */
-       inputLayout.setOnClickListener(new View.OnClickListener() {
+        /* 커뮤니티 */
+        communityLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                showBirthDayPicker();
+
             }
         });
 
@@ -148,89 +132,12 @@ public class MainActivity extends AppCompatActivity {
         webView = (WebView) findViewById(R.id.webView);
         drawerView = (View) findViewById(R.id.drawer);
         pushAlarmLayout = (LinearLayout) findViewById(R.id.push_alarm);
-        inputLayout = (LinearLayout) findViewById(R.id.input_year);
+        communityLayout = (LinearLayout) findViewById(R.id.community_layout);
         appInfoLayout = (LinearLayout) findViewById(R.id.app_info);
         maskCheckLayout = (LinearLayout) findViewById(R.id.mask_checked);
         floatingActionButton = (FloatingActionButton) findViewById(R.id.refresh_btn);
     }
 
-    private void showBirthDayPicker() {
-        Calendar calendar = Calendar.getInstance();
-        int year = calendar.get(Calendar.YEAR);
-
-        final Dialog birthDayDialog = new Dialog(this);
-        birthDayDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        birthDayDialog.setContentView(R.layout.dialog_birthyear);
-
-        Button okBtn = (Button) birthDayDialog.findViewById(R.id.ok_button);
-        Button cancelBtn = (Button) birthDayDialog.findViewById(R.id.cancel_button);
-
-        final NumberPicker picker = (NumberPicker) birthDayDialog.findViewById(R.id.number_picker);
-        picker.setMinValue(year - 100);
-        picker.setMaxValue(year);
-        picker.setDescendantFocusability(NumberPicker.FOCUS_AFTER_DESCENDANTS);
-        setDividerColor(picker, android.R.color.white);
-        picker.setWrapSelectorWheel(false);
-        picker.setValue(year - 20);
-        picker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
-            @Override
-            public void onValueChange(NumberPicker numberPicker, int oldVal, int newVal) {
-
-            }
-        });
-
-        okBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                // 출생년도 저장하기
-                saveRealm(picker.getValue());
-                Toast.makeText(MainActivity.this, "저장되었습니다!", Toast.LENGTH_SHORT).show();
-                birthDayDialog.dismiss();
-            }
-        });
-        cancelBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                birthDayDialog.dismiss();
-            }
-        });
-
-        birthDayDialog.show();
-    }
-
-    /* Realm 에 저장하기 */
-    private void saveRealm(int birthYear){
-        realm = Realm.getDefaultInstance();
-        UserModel userModel = new UserModel();
-
-        realm.beginTransaction();
-        userModel = realm.createObject(UserModel.class);
-        userModel.setBirthYear(birthYear);
-        realm.commitTransaction();
-        Log.d("출생년도", birthYear + "");
-    }
-
-    /* dialog 구분선 커스텀 */
-    private void setDividerColor(NumberPicker picker, int color) {
-        java.lang.reflect.Field[] pickerFields = NumberPicker.class.getDeclaredFields();
-        for (java.lang.reflect.Field pf : pickerFields) {
-            if (pf.getName().equals("mSelectionDivider")) {
-                pf.setAccessible(true);
-                try {
-                    ColorDrawable colorDrawable = new ColorDrawable(color);
-                    pf.set(picker, colorDrawable);
-                } catch (IllegalArgumentException e) {
-                    e.printStackTrace();
-                } catch (Resources.NotFoundException e) {
-                    e.printStackTrace();
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                }
-                break;
-            }
-        }
-    }
 
     /* 위치 권한 설정 */
     private void GPSSetting(){
@@ -325,50 +232,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
-
-
-
-
-    public void notification() {
-
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.HOUR_OF_DAY, 16);
-        calendar.set(Calendar.MINUTE, 5);
-        calendar.set(Calendar.SECOND, 0);
-
-        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
-                .setSmallIcon(R.drawable.applogo)
-                .setContentTitle("올 때 마스크")
-                .setContentText("마스크 구매일입니다! 잊지말고 마스크를 구매해주세요 :)")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setContentIntent(pendingIntent)
-                .setWhen(calendar.getTimeInMillis())
-                .setAutoCancel(true);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            builder.setSmallIcon(R.drawable.applogo);
-            CharSequence charSequence = "Notification Channel";
-            String description = "It's for over Oreo";
-            int importance = NotificationManager.IMPORTANCE_HIGH;
-
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, charSequence, importance);
-            channel.setDescription(description);
-
-            assert notificationManager != null;
-            notificationManager.createNotificationChannel(channel);
-        } else builder.setSmallIcon(R.drawable.applogo);
-
-        assert notificationManager != null;
-        notificationManager.notify(1234, builder.build());
-
+    /* dialog 구분선 커스텀 */
+    private void setDividerColor(NumberPicker picker, int color) {
+        java.lang.reflect.Field[] pickerFields = NumberPicker.class.getDeclaredFields();
+        for (java.lang.reflect.Field pf : pickerFields) {
+            if (pf.getName().equals("mSelectionDivider")) {
+                pf.setAccessible(true);
+                try {
+                    ColorDrawable colorDrawable = new ColorDrawable(color);
+                    pf.set(picker, colorDrawable);
+                } catch (IllegalArgumentException e) {
+                    e.printStackTrace();
+                } catch (Resources.NotFoundException e) {
+                    e.printStackTrace();
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+                break;
+            }
+        }
     }
+
 
     private class WebViewClientClass extends WebViewClient {
         @Override
